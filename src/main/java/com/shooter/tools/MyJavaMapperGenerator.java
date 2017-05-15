8524a3e9f61228c947f358d05a4c4212546abea3
visionsky <visionsky@vision-PC>
2017-05-15 23:39:40 +0800

    update

---
+++ src/main/java/com/shooter/tools/MyJavaMapperGenerator.java
@@ -0,0 +1,7 @@
+package com.shooter.tools;
+
+import org.mybatis.generator.codegen.mybatis3.javamapper.JavaMapperGenerator;
+
+public class MyJavaMapperGenerator extends JavaMapperGenerator {
+
+}
---
+++ src/main/java/com/shooter/tools/MyXMLMapperGenerator.java
@@ -0,0 +1,7 @@
+package com.shooter.tools;
+
+import org.mybatis.generator.codegen.mybatis3.xmlmapper.XMLMapperGenerator;
+
+public class MyXMLMapperGenerator extends XMLMapperGenerator {
+
+}
